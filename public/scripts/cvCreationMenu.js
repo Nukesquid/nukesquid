ddb8dd9b45330bb4bf15924682dfb4a0b7962679
Alexander Kjeserud <ole.alexander89@gmail.com>
2015-03-23 01:02:21 +0100

    added json to mysql parsing for inserts

--- public/scripts/cvCreationMenu.js
+++ public/scripts/cvCreationMenu.js
@@ -65,8 +65,8 @@ var currentView;
 var createCvMenu = document.getElementById('createCvMenu');
 var createCvMenu = createCvMenu.getElementsByTagName('li');
 // create introForm
-var forsideTxt = {btn:createCvMenu[0], 
-				  form:'intro', 
+var hovedSide = {btn:createCvMenu[0], 
+				  form:'main', 
 				  responseHandler : function(dom){ // handles recived dom from ajax call
 					//write all tags til list
 				 	var list = dom.getElementsByTagName('ul')[0];
@@ -81,7 +81,7 @@ var forsideTxt = {btn:createCvMenu[0],
 						list.appendChild(listElement);
 					}
 					dom.getElementsByTagName('button')[0].addEventListener('click', function(e){
-						sendJson.createCv[0].mainCv.cvName = dom.getElementsByTagName('input')[0];
+						sendJson.createCv[0].mainCv.cvName = dom.getElementsByTagName('input')[0].value;
 						sendJson.createCv[0].mainCv.introTxt = dom.getElementsByTagName('textarea')[0].value;
 						sendJson.author = loginId;
 						sendJson.createCv[0].user = consultant;
@@ -96,33 +96,16 @@ var forsideTxt = {btn:createCvMenu[0],
 						postToServer(sendJson);
 				 })}};
 // WORK IN PROGRESS!!!
-var hovedside = {btn:createCvMenu[1], 
-				 form: 'mainpage',
+var utdanning = {btn:createCvMenu[1], 
+				 form: 'edu',
 				 responseHandler : function (dom) {
-					//write all tags til list
-				 	var list = dom.getElementsByTagName('ul')[0];
-					var tags = getTags();
-					for (i=0; i<tags.length;i++){
-						var listElement = document.createElement('li');
-						var checkbox = document.createElement('input');
-						checkbox.type = 'checkbox';
-						checkbox.value = tags[i].tagId;
-						listElement.appendChild(checkbox);
-						listElement.innerHTML += ' ' + tags[i].tagName + ' | ' + tags[i].tagCategoryName;
-						list.appendChild(listElement);
-					}
 					// place selected items in sendJson	
 					dom.getElementsByTagName('button')[0].addEventListener('click', function(e){
 						sendJson.author = loginId;
 						sendJson.createCv[0].user = consultant;
-						var listElements = list.childNodes;
-						for (i=0;i<listElements.length;i++){
-							var checkbox = listElements[i].getElementsByTagName('input')[0];
-							if (checkbox.checked){
-								sendJson.createCv[0].cvMain[sendJson.createCv[0].cvMain.length] = checkbox.value;
-							}
-						}
-						console.log(sendJson);
+						var inputs = dom.getElementsByTagName('input');
+						sendJson.createCv[0].edu.grad = inputs[0].value						
+						sendJson.createCv[0].edu.sted = inputs[1].value;
 						postToServer(sendJson);
 					})}};
 var erfaringer = {btn : createCvMenu[2],
@@ -144,17 +127,17 @@ var erfaringer = {btn : createCvMenu[2],
 						var inputs = dom.getElementsByTagName('input');
 						sendJson.author = loginId;
 						sendJson.createCv[0].user = consultant;
-						sendJson.createCv[0].cvExperience[0].role = inputs[0];
-						sendJson.createCv[0].cvExperience[0].role = inputs[1]
- 						sendJson.createCv[0].cvExperience[0].time.from = inputs[2];
-						sendJson.createCv[0].cvExperience[0].time.to = inputs[3];
-						sendJson.createCv[0].cvExperience[0].body = inputs[4];
+						sendJson.createCv[0].cvExperience.role = inputs[0].value;
+						sendJson.createCv[0].cvExperience.client = inputs[1].value;
+ 						sendJson.createCv[0].cvExperience.from = inputs[2].value;
+						sendJson.createCv[0].cvExperience.to = inputs[3].value;
+						sendJson.createCv[0].cvExperience.body = dom.getElementsByTagName('textarea')[0].value;
 						// add tags
 						var listElements = list.childNodes;
 						for (i=0;i<listElements.length;i++){
 							var checkbox = listElements[i].getElementsByTagName('input')[0];
 							if (checkbox.checked){
-								sendJson.createCv[0].cvExperience[0].tags[sendJson.createCv[0].cvExperience[0].tags.length] = checkbox.value;
+								sendJson.createCv[0].cvExperience.tags[sendJson.createCv[0].cvExperience.tags.length] = checkbox.value;
 							}
 						}
 						console.log(sendJson);
@@ -162,8 +145,8 @@ var erfaringer = {btn : createCvMenu[2],
 					})}};
 
 // createCvmenu functionality
-addBtnEvent(forsideTxt);
-addBtnEvent(hovedside);
+addBtnEvent(hovedSide);
+addBtnEvent(utdanning);
 addBtnEvent(erfaringer);
 
 /*
--- routs/controller.js
+++ routs/controller.js
@@ -7,11 +7,64 @@
 var express = require('express');
 var app = express();
 var bp = require('body-parser');
+// mysql connection
+sql = require('mysql').createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : 'kakeboks',
+	database : 'cvApp'
+});
 
 
+
+function inserts(author, obj){
+	console.log(obj);
+	for (i=0;i<obj.length;i++){
+		this.user = obj[i].user;
+		if (obj[i].mainCv.cvNavn !== '' && obj[i].mainCv.introTxt !== '' && obj[i].mainCv.cvTags.length !== 0){
+			this.cvTags = obj[i].mainCv.cvTags;
+			sql.query('INSERT INTO Cv SET ?', {/*createdBy: author,*/ 
+				brukerId: obj[i].user, 
+				cvIntroduksjon : obj[i].mainCv.introTxt, 
+				cvNavn: obj[i].mainCv.cvName}, function (err, result){
+					for (l=0;l<cvTags.length;l++){
+						sql.query('INSERT INTO TeknologiLink SET ?', {cvId: result.insertId,
+							teknologiId: cvTags[l]});
+				}
+			});	
+		}
+		if (obj[i].edu.sted !== '' && obj[i].edu.grad !== ''){
+			sql.query('INSERT INTO Utdanning SET ?', {brukerId : this.user,
+				utdanningSted : obj[i].edu.sted,
+				utdanningGrad : obj[i].edu.grad});
+		}
+		if (obj[i].cvExperience.role !== '' && obj[i].cvExperience.client !== '' && obj[i].cvExperience.from !== '' &&
+			obj[i].cvExperience.to !== '' && obj[i].cvExperience.body !== '' && obj[i].cvExperience.tags.length !== 0){
+			this.ExpTags = obj[i].cvExperience.tags;
+			sql.query('INSERT INTO Referanser SET ?', {brukerId : this.user,
+				referanseTekst : obj[i].cvExperience.body,
+				tidFra : obj[i].cvExperience.from, 
+				tidTil : obj[i].cvExperience.to,
+				referanseRolle : obj[i].cvExperience.role,
+				kundeId : obj[i].cvExperience.client}, function (err, result){
+					for (l=0;l<ExpTags.length;l++){
+						sql.query('INSERT INTO TeknologiLink SET ?', {referanseId: result.insertId,
+							teknologiId: ExpTags[l]});
+					}
+				});
+		}
+		
+	}
+}
+
+function dataParser(json){
+	console.log('dataParser sayes hi');
+	inserts(json.author,json.createCv);
+}
 
 app.post('/', bp.json(), function(req, res){
-	console.log(req.body);
+	dataParser(req.body);
+	//console.log(req.body);
 });
 
 module.exports = app;
